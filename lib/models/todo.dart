d8cbb0f139a1ec9b5303f5d1b54e5b4465492f12
Bedbug10 <bonombelleaurelien08@gmail>
2022-07-12 15:21:56 +0100

    ajout de la fonction qui recolter le price avec regrex

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -2,18 +2,17 @@ class  Todo {
   final int? id ; 
   final int? taskId ; 
  final String? title ; 
+ final int? price; 
 
- final int? isDone; 
 
-
-  Todo({this.id ,this.taskId, this.title,this.isDone});  
+  Todo({this.id ,this.taskId, this.title,this.price});  
 
   Map<String,dynamic> toMap(){ 
     return { 
       'id': id, 
       "taskId": taskId, 
       "title": title, 
-      "isDone": isDone, 
+      "price": price, 
     }; 
   } 
 }
